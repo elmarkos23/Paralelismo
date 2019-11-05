60260972b5f84024d8069a10359d70151c10ba1f
Markos Ayala <elmarkos23@gmail.com>
2019-11-05 17:39:25 -0500

    commit inicial

---
+++ Paralelismo/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paralelismo
+{
+ public class Persona
+  {
+    public int id { get; set; }
+    public string Name { get; set; }
+  }
+}
---
+++ Paralelismo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paralelismo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Persona> listaPersona = new List<Persona>();
+
+      for (int i = 100; i <= 999; i++)
+      {
+        listaPersona.Add(new Persona { id = i, Name = "Marco" });
+      }
+
+      var options = new ParallelOptions()
+      {
+        MaxDegreeOfParallelism = 2
+      };
+      Parallel.ForEach(listaPersona, x =>
+      {
+        Console.WriteLine(@"value of i = {0}, thread = {1}, time={2}",x.id, Thread.CurrentThread.ManagedThreadId,DateTime.Now.ToString("HH:mm:ss"));
+      });
+      Console.WriteLine("************");
+      Console.WriteLine("************");
+      Console.WriteLine("************");
+      foreach (Persona persona in listaPersona)
+      {
+        Console.WriteLine(@"value of i = {0}, thread = {1}, time={2}", persona.id, Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToString("HH:mm:ss"));
+      }
+
+      Console.WriteLine("Press any key to exist");
+      Console.ReadLine();
+    }
+  }
+}
