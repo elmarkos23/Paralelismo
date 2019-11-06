c53c940395f5aa8cca9705140678c2af7e0f39dd
Markos Ayala <elmarkos23@gmail.com>
2019-11-06 08:31:23 -0500

    Almacenamiento provicional

--- Paralelismo/Program.cs
+++ Paralelismo/Program.cs
@@ -17,18 +17,19 @@ namespace Paralelismo
       {
         listaPersona.Add(new Persona { id = i, Name = "Marco" });
       }
-
-      var options = new ParallelOptions()
-      {
-        MaxDegreeOfParallelism = 2
-      };
+      
+      //var options = new ParallelOptions()
+      //{
+      //  MaxDegreeOfParallelism = 2
+      //};
+      Console.WriteLine("*** Proceso Parallel.ForEach ***");
       Parallel.ForEach(listaPersona, x =>
       {
         Console.WriteLine(@"value of i = {0}, thread = {1}, time={2}",x.id, Thread.CurrentThread.ManagedThreadId,DateTime.Now.ToString("HH:mm:ss"));
       });
-      Console.WriteLine("************");
-      Console.WriteLine("************");
-      Console.WriteLine("************");
+
+      Console.WriteLine("\n\n\n");
+      Console.WriteLine("*** Proceso Normal foreach ***");
       foreach (Persona persona in listaPersona)
       {
         Console.WriteLine(@"value of i = {0}, thread = {1}, time={2}", persona.id, Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToString("HH:mm:ss"));
